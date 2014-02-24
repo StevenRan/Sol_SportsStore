5e45d1b7d95a9af017a6a38b53686f38595e03f1
Steven Ran <rankun@hotmail.com>
2014-02-24 13:43:38 +0800

    Init - Adding all files to new repository

---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using SportsStore.Domain.Abstract;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        //
+        // GET: /Product/
+
+        private IProductRepository repo;
+        private int pageSize = 2;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            this.repo = productRepository;
+        }
+
+        public ViewResult List(int page = 1)
+        {
+            return View(repo.Products
+                .OrderBy(p=>p.ProductId)
+                .Skip((page-1) * pageSize)
+                .Take(pageSize));
+        }
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+using SportsStore.Domain.Entities;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
+using Moq;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        private static IKernel kernal = new StandardKernel();
+
+        public NinjectControllerFactory()
+        {
+            AddBings();
+        }
+
+        protected override IController GetControllerInstance(System.Web.Routing.RequestContext requestContext, Type controllerType)
+        {
+            return (controllerType==null)? null: (IController)kernal.TryGet(controllerType);
+        } 
+
+
+        public void AddBings()
+        {
+            kernal.Bind<IProductRepository>().To<EFProductRepository>();
+            //Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            //mock.Setup( m => m.Products).Returns(new List<Product>{
+            //new Product {Name = "Football", Price = 25},
+            //new Product {Name = "Surf board", Price =179}
+            //}.AsQueryable());
+
+            //kernal.Bind<IProductRepository>().ToConstant(mock.Object);
+        }
+    }
+}
